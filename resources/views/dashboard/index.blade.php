974a3c761c818be0053167b304e9b478a38069b1
aayyiik <ari.lathifatul.chusna-2020@vokasi.unair.ac.id>
2022-02-07 21:51:52 +0700

    setup

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,18 @@
+@extends('templates.master')
+@section('content')
+
+<main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Blank Page</h1>
+   
+        </div><!-- End Page Title -->
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Example Card</h5>
+                    <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+                </div>
+            </div>
+  </main>
+    
+@endsection
